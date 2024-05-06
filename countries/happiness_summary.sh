45e8937fc8bab5f8ccc6a060a06900b11053a26c
ajaysingh-codes <ajay.singh@sjsu.edu>
2024-05-06 04:55:05 -0400

    Added some new summaries

--- countries/happiness_summary.sh
+++ countries/happiness_summary.sh
@@ -11,12 +11,14 @@ then
 fi
 
 # Print the header
-echo "Country,Avg Happiness,Avg GDP,Avg Life Expectancy"
+echo "Country,Avg Happiness,Avg GDP,Avg Life Expectancy,Avg Freedom"
 
 #Initialize the variables
 sum_happiness=0
 sum_gdp=0
 sum_life=0
+sum_freedom=0
+
 count=0
 
 # Read file line by line
@@ -28,6 +30,8 @@ do
 		sum_happiness=$(echo "$sum_happiness + $happiness" | bc -l)
 		sum_gdp=$(echo "$sum_gdp + $gdp" | bc -l)
 		sum_life=$(echo "$sum_life + $life_expectancy" | bc -l)
+		sum_freedom=$(echo "$sum_freedom + $freedom" | bc -l)
+	
 		count=$((count + 1))
 	fi
 done < "$file"
@@ -36,9 +40,10 @@ done < "$file"
 mean_happiness=$(echo "scale=2; $sum_happiness / $count" | bc)
 mean_gdp=$(echo "scale=2; $sum_gdp / $count" | bc)
 mean_life=$(echo "scale=2; $sum_life / $count" | bc)
+mean_freedom=$(echo "scale=2; $sum_freedom / $count" | bc)
 
 # Print results
-echo "$country,$mean_happiness,$mean_gdp,$mean_life"
+echo "$country,$mean_happiness,$mean_gdp,$mean_life,$mean_freedom"
 	
 exit 0
 
